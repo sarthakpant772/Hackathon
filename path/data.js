cbc80328d7f987f849d11e692365f53505e87a44
“Bhavya <singhpappu966@gmail.com>
2021-10-23 16:57:19 +0530

    changed form

--- path/data.js
+++ path/data.js
@@ -1,5 +1,9 @@
-const data = JSON.parse[
-    {title:"Testing STUFFFF", content: "this is web dev"}
+const obj = [
+    '{"title":"Testing STUFFFF", "content": "this is web dev"}',
+    '{"title":"NEWWWW STUFFDDDD", "content": "this is a TESTING DATAS"}'
 ];
+
+const data = JSON.parse(obj);
+
 
 document.getElementById("test").innerHTML = data.title;
